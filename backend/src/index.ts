0ff5cc78b454656903bfe72a8463c9c728061d29
Guilherme Storch de Oliveira <GSOliveira@brasal.com.br>
2026-06-28 14:56:06 -0300

    chore(backend): install and register CORS plugin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
 
 const app = new Elysia()
+  .use(cors())
   .get('/', () => 'TechGraph Backend is Running')
   .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+
+export { app };
